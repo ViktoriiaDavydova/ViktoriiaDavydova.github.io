4ba713179b15c762c06bc2a7fc54a1b1a3b73df3
ViktoriiaDavydova <44687083+HakunaMatat@users.noreply.github.com>
2020-09-08 12:15:38 -0600

    Updates

---
+++ precache-manifest.d7354dcde945e6eb337170e1267f5412.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a1c0a6fdac8ae6fa3a3c4e6a55913a",
+    "url": "/vdavydova-portfolio/index.html"
+  },
+  {
+    "revision": "c4881167bdd5b514a6be",
+    "url": "/vdavydova-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a7d34bd241ca02ddc981",
+    "url": "/vdavydova-portfolio/static/js/2.08a38389.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vdavydova-portfolio/static/js/2.08a38389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4881167bdd5b514a6be",
+    "url": "/vdavydova-portfolio/static/js/main.1bf0cea1.chunk.js"
+  },
+  {
+    "revision": "8bf2a2a051f461e2aea4",
+    "url": "/vdavydova-portfolio/static/js/runtime-main.86353677.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vdavydova-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
