0ff1fa363f102a461454f1d662fd999e686e756e
V Davydova <44687083+HakunaMatat@users.noreply.github.com>
2021-03-23 13:25:17 -0600

    Commit message

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,46 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
+import { IconContext } from "react-icons";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <>
-        <div>
-          <nav className="nav-menu active">
-            <div className="nav-menu-items">
-              <div className="navbar-toggle">
-                <img
-                  className="aboutMeImg"
-                  src={require("../images/aboutMeImg.jpg")}
-                  alt="Some nature"
-                />
-                <div className="frames">
-                  <div> VIKTORIIA DAVYDOVA </div>
-                  <div>Front-End Web Developer</div>
-                </div>
-              </div>
-              {SidebarData.map((item, index) => {
-                return (
-                  <li key={index} className={item.cName}>
-                    <Link to={item.path}>{item.title}</Link>
-                  </li>
-                );
-              })}
-            </div>
-          </nav>
+function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+  return (
+    <>
+      <div>
+        <div className="navbar">
+          <Link to="#" className="menu-bars">
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
         </div>
-      </>
-    );
-  }
+        <nav className="nav-menu active">
+          <div className="nav-menu-items">
+            <div className="navbar-toggle">
+              <img
+                className="aboutMeImg"
+                src={require("../images/aboutMeImg.jpg")}
+                alt="Some nature"
+              />
+              <div className="frames">
+                <div> VIKTORIIA DAVYDOVA </div>
+                <div>Front-End Web Developer</div>
+              </div>
+            </div>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>{item.title}</Link>
+                </li>
+              );
+            })}
+          </div>
+        </nav>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
